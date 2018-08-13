f042f3d1a1f14b3d1a5a64eefc30bf9115888387
WillianDeCastro <oilha2010@hotmail.com>
2018-08-12 22:06:51 -0300

    incluída mensagem toast

--- app/src/main/java/com/example/oilha/adivinha/MainActivity.java
+++ app/src/main/java/com/example/oilha/adivinha/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
@@ -16,8 +17,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void InformaValor(View view) {
-
-
         ((TextView)findViewById(R.id.txtResulta)).setText(new Numeros().MensagemFinal());
+
+        Toast.makeText(getApplicationContext(),"Mais sorte da próxima", Toast.LENGTH_LONG).show();
     }
 }
